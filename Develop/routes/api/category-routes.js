27cb7d6c97039d0bc10b42e73f0e7c8fd384839a
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-23 13:39:28 -0600

    Get All-Categories and Get Category By Id routes added

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,16 +1,36 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories
+    const categoryData = await Category.findAll({
+      // Include associated Products
+      include: [
+        { model: Product }
+      ]
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // find one category by its `id` value
+    const categoryData = await Category.findByPk(req.params.id, {
+      // Include associated Products
+      include: [
+        { model: Product }
+      ]
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
